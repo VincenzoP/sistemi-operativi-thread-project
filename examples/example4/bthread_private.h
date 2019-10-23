5e76facfa1878eef591239e13f5db10a6b62b588
VincenzoP <kurovaan@gmail.com>
2019-10-23 10:40:54 +0200

    btrhread private

---
+++ examples/example4/bthread_private.h
@@ -0,0 +1,11 @@
+typedef struct
+{
+    bthread_t tid;
+    bthread_routine body;
+    void *arg;
+    bthread_state state;
+    bthread_attr_t attr;
+    char *stack;
+    jmp_buf context;
+    void *retval;
+} __bthread_private;
